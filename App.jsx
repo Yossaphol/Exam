d9b9587f4550db9fdd7c0147989af7b22d187e8f
Yossaphol <69375434+Yossaphol@users.noreply.github.com>
2024-02-04 22:03:58 +0700

    App.jsx

--- App.jsx
+++ App.jsx
@@ -3,15 +3,21 @@ import { useState, useEffect } from 'react'
 import './App.css'
 import Header from './components/Headerss'
 import Login from './login'
+import Forms from './Forms/cj'
+import { BrowserRouter ,Routes ,Route } from 'react-router-dom'
 
 function App() {
 
   return (
     <>
       <div className='font-link'>
-      <Header></Header>
-      {/* <Login></Login> */}
-
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<Header/>}></Route>
+          <Route path='/login' element={<Login/>}></Route>
+          <Route path='/Forms/cj' element={<Forms/>}></Route>
+        </Routes>
+      </BrowserRouter>
       </div>
     </>
   )
